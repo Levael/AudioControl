e3c07c44f4f5b3db52521a9f0ef11f0db3e51207
Levael Savvy <Levael.Savvy@gmail.com>
2023-12-07 01:13:23 +0200

    in progress

--- AudioControl/Program.cs
+++ AudioControl/Program.cs
@@ -1,4 +1,119 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+class NamedPipeServer
+{
+    private const string PipeName = "AudioPipe";
+    private NamedPipeServerStream pipeServer;
+    private StreamReader streamReader;
+    private StreamWriter streamWriter;
+    private CancellationTokenSource cts = new CancellationTokenSource();
+
+    public NamedPipeServer()
+    {
+        pipeServer = new NamedPipeServerStream(PipeName, PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+        streamReader = new StreamReader(pipeServer);
+        streamWriter = new StreamWriter(pipeServer);
+    }
+
+    public async Task StartAsync(int parentProcessId)
+    {
+        var parentProcess = Process.GetProcessById(parentProcessId);
+        var monitorThread = new Thread(() =>
+        {
+            while (!parentProcess.HasExited) { Thread.Sleep(500); }
+            Environment.Exit(0); // if parent process died -> close the program
+        });
+        monitorThread.Start();
+
+
+        Console.WriteLine("Ожидание подключения клиента...");
+        await pipeServer.WaitForConnectionAsync(cts.Token);
+
+        Console.WriteLine("Клиент подключен. Начинаем мониторинг входящих сообщений.");
+        while (!cts.Token.IsCancellationRequested)
+        {
+            try
+            {
+                string message = await streamReader.ReadLineAsync();
+                if (message != null)
+                {
+                    Console.WriteLine($"Получено сообщение: {message}");
+                    string response = ProcessMessage(message);
+                    await streamWriter.WriteLineAsync(response);
+                    await streamWriter.FlushAsync();
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Ошибка ввода/вывода: {ex.Message}");
+                break;
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Операция отменена.");
+                break;
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        cts.Cancel();
+    }
+
+    private string ProcessMessage(string message)
+    {
+        // Здесь ваша логика обработки сообщения
+        return $"Обработано: {message}";
+    }
+
+    public void Dispose()
+    {
+        streamReader?.Dispose();
+        streamWriter?.Dispose();
+        pipeServer?.Dispose();
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Parent process ID is required.");
+            return;
+        }
+
+        if (!int.TryParse(args[0], out int parentProcessId))
+        {
+            Console.WriteLine("Invalid parent process ID.");
+            return;
+        }
+
+
+        var server = new NamedPipeServer();
+        await server.StartAsync(parentProcessId);
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+
+        server.Stop();
+        server.Dispose();
+    }
+}
+
+
+
+
+
+/*using System.Diagnostics;
+using System.IO;
 using System.IO.Pipes;
 using NAudio.CoreAudioApi;
 
@@ -19,46 +134,62 @@ class AudioControlApp
         var parentProcess = Process.GetProcessById(parentProcessId);
         var monitorThread = new Thread(() =>
         {
-            while (!parentProcess.HasExited)    Thread.Sleep(1000);
-            Environment.Exit(0);
+            while (!parentProcess.HasExited) { Thread.Sleep(500); }
+            Environment.Exit(0);    // if parent process died -> close the program
         });
         monitorThread.Start();
-        /*Thread.Sleep(1000);
-        //Console.WriteLine("started");
-        Console.Beep(800, 500);
-        Thread.Sleep(3000);
-        //Console.WriteLine("continued");
-        Console.Beep(800, 500);
-        Thread.Sleep(1000);*/
 
-        while(true)
+
+        var debugThread = new Thread(() =>
         {
-            Console.Beep(800, 200);
-            Thread.Sleep(500);
-        }
-
-
-
-
-        /*var processor = new AudioCommandProcessor();
-        using (var pipeServer = new NamedPipeServerStream("AudioPipe", PipeDirection.InOut))
-        {
-            pipeServer.WaitForConnection();
-
-            using (var streamReader = new StreamReader(pipeServer))
-            using (var streamWriter = new StreamWriter(pipeServer))
-            {
-                *//*while (true)
-                {
-                    var command = streamReader.ReadLine();
-                    if (command == null || command == "Exit") break; // "null" means closing of named pipe
-
-                    var response = processor.ProcessCommand(command);
-                    streamWriter.WriteLine(response);
-                    streamWriter.Flush();
-                }*//*
+            while (true) {
+                Console.Beep(800, 100);
+                Thread.Sleep(250);
             }
-        }*/
+        });
+        debugThread.Start();
+
+
+        var processor = new AudioCommandProcessor();
+        var pipeServer = new NamedPipeServerStream("AudioPipe", PipeDirection.InOut);
+        pipeServer.WaitForConnection();
+
+        var streamReader = new StreamReader(pipeServer);
+        var streamWriter = new StreamWriter(pipeServer);
+
+        Task.Run(() => { Console.Beep(1800, 700); });
+
+        
+        var readThread = new Thread(async () =>
+        {
+            while (true)
+            {
+                try
+                {
+                    string command = await streamReader.ReadLineAsync();
+                    if (command != null)
+                    {
+                        var response = processor.ProcessCommand(command);
+
+                        *//*streamWriter.WriteLine($"Response from server: {response}");
+                        streamWriter.Flush();*//*
+                        //Task.Run(async  () => { 
+                        Console.Beep(1500, 100);
+
+                        await streamWriter.WriteLineAsync($"Response from server: {response}");
+                        await streamWriter.FlushAsync();
+                        //});
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Task.Run(() => { Console.Beep(200, 1000); });
+                    break;
+                }
+            }
+        });
+
+        readThread.Start();
     }
 
 }
@@ -76,14 +207,11 @@ class AudioCommandProcessor
             case "GetAudioDevices":
                 return GetAudioDevices();
             case "StartStreaming":
-                StartStreaming();
-                return "Streaming Started";
+                return StartStreaming();
             case "StopStreaming":
-                StopStreaming();
-                return "Streaming Stopped";
+                return StopStreaming();
             case "PlayAudioFile":
-                PlayAudioFile();
-                return "Audio File Playing";
+                return PlayAudioFile();
             default:
                 return "Unknown Command";
         }
@@ -101,18 +229,19 @@ class AudioCommandProcessor
         return response;
     }
 
-    private void StartStreaming()
+    private string StartStreaming()
     {
-        
+        return "Streaming Started";
     }
 
-    private void StopStreaming()
+    private string StopStreaming()
     {
-        
+        return "Streaming Stopped";
     }
 
-    private void PlayAudioFile()
+    private string PlayAudioFile()
     {
-        
+        return "Audio File Playing";
     }
 }
+*/
